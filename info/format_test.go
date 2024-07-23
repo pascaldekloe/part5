27af6fbef46a8cee485dd0893292ad5ea95303b8
Pascal S. de Kloe <pascal@quies.net>
2024-07-23 18:22:08 +0200

    format fuzzer

--- info/format_test.go
+++ info/format_test.go
@@ -2,6 +2,7 @@ package info
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
@@ -44,4 +45,40 @@ func TestGoldenFormats(t *testing.T) {
 				gold.format, gold.value, got, gold.want)
 		}
 	}
+}
+
+func FuzzDataUnitFormat(f *testing.F) {
+	f.Fuzz(func(t *testing.T, typeID byte, enc byte, cause byte, origAddr byte, comAddr byte, info []byte) {
+		u := DataUnit[OrigAddr8, ComAddr8, ObjAddr8]{
+			Type:  TypeID(typeID),
+			Enc:   Enc(enc),
+			Cause: Cause(cause),
+			Orig:  OrigAddr8{origAddr},
+			Addr:  ComAddr8{comAddr},
+			Info:  info,
+		}
+		got := fmt.Sprintf("%s\n%#s", &u, u)
+		a, b, _ := strings.Cut(got, "\n")
+		if a == "" || b == "" {
+			t.Fatalf("got %q and %q", a, b)
+		}
+
+		// Check the ending first to be reasonable sure of a complete formatting.
+		switch a[len(a)-1] {
+		case '.', '?', '!':
+			if a[len(a)-1] != b[len(b)-1] {
+				t.Errorf("last character differs between %q and %q", a, b)
+			}
+		default:
+			t.Errorf("last character not '.', '?' or '!' of %q (with %q)", a, b)
+		}
+
+		// ASDUs have a fixed data-unit identifier.
+		switch DUI, _, _ := strings.Cut(a, ":"); {
+		case DUI == "":
+			t.Errorf("want data-unit identifier terminated by ':' in %q", a)
+		case strings.Count(DUI, " ") != 3:
+			t.Errorf("data-uint identifier %q of %q does not have 4 fields", DUI, a)
+		}
+	})
 }
