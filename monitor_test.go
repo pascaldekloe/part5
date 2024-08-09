5549d661cef2ff1733c214462e308625af76110c
Pascal S. de Kloe <pascal@quies.net>
2024-08-09 17:39:52 +0200

    formatting error inclusion in fuzz check

--- monitor_test.go
+++ monitor_test.go
@@ -1,7 +1,7 @@
 package part5
 
 import (
-	"io"
+	"bytes"
 	"testing"
 
 	"github.com/pascaldekloe/part5/info"
@@ -10,11 +10,17 @@ import (
 // Be resillient against malicious and faulty ASDU.
 func FuzzMonitorWideASDU(f *testing.F) {
 	var sys info.Params[info.OrigAddr8, info.ComAddr16, info.ObjAddr24]
-	mon := NewMonitorDelegateDefault(NewLogger(sys, io.Discard))
 
-	f.Fuzz(func(t *testing.T, bytes []byte) {
+	f.Fuzz(func(t *testing.T, asdu []byte) {
+		var buf bytes.Buffer
+		mon := NewMonitorDelegateDefault(NewLogger(sys, &buf))
+
 		u := sys.NewDataUnit()
-		u.Adopt(bytes)
+		u.Adopt(asdu)
 		MonitorDataUnit(mon, u)
+
+		if bytes.IndexByte(buf.Bytes(), '!') >= 0 {
+			t.Error("got formatting error in logger output:", buf)
+		}
 	})
 }
